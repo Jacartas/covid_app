db36b1500e1ab4bc03a22b5fcb16e51041adb9ea
tomas-silva-1 <61935208+tomas-silva-1@users.noreply.github.com>
2020-07-29 12:48:48 +0100

    add some symptoms design all still provisory

--- lib/pages/symptoms.dart
+++ lib/pages/symptoms.dart
@@ -11,13 +11,65 @@ class Symptoms extends StatelessWidget {
           Text('Symptoms',
           textAlign: TextAlign.center,
           style: TextStyle(
-              fontSize: 20,
+              fontSize: 30,
             ),
           ),
-          Row(
-            children: <Widget>[
-              Icon(Icons.sentiment_dissatisfied, size: 20,),
-            ],
+          SizedBox(height: 20,),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 15, 0, 10),
+            child: Text('Most Common Symptoms:',
+              style: TextStyle(
+              fontSize: 25,
+             ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.fromLTRB(15, 0, 0, 10),
+            child: Text('fever \ndry cough \ntiredness',
+              style: TextStyle(
+                fontSize: 20,
+                height: 1.1,
+
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+            child: Text('Less Common Symptoms:',
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(15, 0, 0, 10),
+            child: Text('aches and pains \nsore throat \ndiarrhoea \nconjunctivitis \nheadache'
+                ' \nloss of taste or smell \na rash on skin, or discolouration of fingers or toes',
+              style: TextStyle(
+                fontSize: 20,
+                height: 1.1,
+
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+            child: Text('Serious Symptoms:',
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(15, 0, 0, 10),
+            child: Text('difficulty breathing or shortness of breath \nchest pain or pressure \nloss of speech or movement',
+              style: TextStyle(
+                fontSize: 20,
+                height: 1.1,
+
+              ),
+            ),
           ),
         ],
       ),
